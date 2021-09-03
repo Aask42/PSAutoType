afc5f9b2bd10afa7870b7a4e9f23e452baee0672
unknown <ask.wietting@gmail.com>
2021-09-02 21:19:59 -0500

    Adding basic code

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # PSAutoType
+
 This is meant to be a simple tool for creating sets of automated HID inputs and having PowerShell "type" them out. 
+
+## Example Usage
+
+```powershell
+.\autokey.ps1 "hello world"
+```
---
+++ autokey.ps1
@@ -0,0 +1,40 @@
+<#
+.AUTHOR
+    AASK 20210902
+.EXAMPLE
+    autokey.ps1 "action"
+.NOTES
+    Don't forget to make your paste.txt file
+#>
+
+$action = $args[0]
+
+$paste = get-content ./paste.txt # Get our paste file as ps hates typing special characters
+set-clipboard $paste
+
+$actions = @{
+    "hello_world" = @('Hello World','{enter}','^v')
+}
+
+# Validate we have this action
+if(!$actions.$action){
+    write-host "$action does not exist, sorry charlie!"
+    exit
+}
+
+$wshell = New-Object -ComObject wscript.shell;
+
+notepad.exe
+$wshell.AppActivate('Untitled - Notepad') # Select the right window in case we got clicked off
+start-sleep 1
+# Input all our freakin' commands
+for($i = 0;$i -lt $actions.$action.Count;$i++){
+    if($actions.$action[$i][0] -eq '{' -or $actions.$action[$i][0] -eq '^'){
+        $wshell.SendKeys($actions.$action[$i])
+        start-sleep -milliseconds 25
+    } else {
+        for($x = 0;$x -lt $actions.$action[$i].Length;$x++ ){
+            $wshell.SendKeys($actions.$action[$i][$x])
+        }
+    }
+}
